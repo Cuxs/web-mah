8002a985dd1b7deed48739ce1f5a737592287d3a
Mariano Fliri <mfliri@as-one.com.ar>
2018-01-15 12:32:26 -0300

    Full filter functionality

---
+++ src/stories/ActiveFilters.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import _ from 'lodash';
+import { split } from 'split-object';
+import { stringify } from 'query-string';
+/* eslint react/jsx-filename-extension: 0 */
+
+
+/* En caso de querer agregar un filtro, los archivos que se tienen que modificar son:
+ActiveFilters (agregar el filtro al if)
+FilterLists (Agregar el parseo correspondiente)
+Modificar en el api el switch de rounter/index.js:398 y el metodo en general getFiltersAndTotalResult
+Modificar la mutacion tanto en la api(dentro de PublicationTypes) como en ApolloQueries */
+
+export default ({ searchData }) => split(searchData).map((filter) => {
+  if (filter.key === 'fuel' ||
+      filter.key === 'year' ||
+      filter.key === 'state') {
+    return (
+      <Button
+        style={{ cursor: 'pointer' }}
+        name={filter.value}
+        onClick={() => {
+            delete searchData[filter.key];
+            window.location.assign(`/SearchCars?${stringify(searchData)}`);
+          }}
+      >
+        {filter.value}
+      </Button>);
+  }
+});
--- src/stories/filtersList.js
+++ src/stories/filtersList.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { split } from 'split-object';
 /* eslint react/jsx-filename-extension: 0 */
 
-
-const items = value => (
+let href = '#';
+const items = (title, value, search) => (
   <div>
-    {split(value).map(row => (
-      <p className="option">
-        {row.key} <span className="quantity">({row.value})</span>
-      </p>
-    ))}
+    {split(value).map((row) => {
+    href = split(value).length === 1 ? '#' : `${search}&${title}=${row.key}`; // deshabilita el href si es solo una opcion
+      return (
+        <a className="option" href={href}>
+          {row.key} <span className="quantity">({row.value})</span>
+        </a>
+    );
+})}
   </div>
 );
 const parseTitle = (title) => {
@@ -18,8 +21,6 @@ const parseTitle = (title) => {
       return 'Combustible';
     case 'year':
       return 'Año';
-    case 'carState':
-      return 'Estado del auto';
     case 'state':
       return 'Estado de la publicación';
     default:
@@ -29,7 +30,7 @@ const parseTitle = (title) => {
 export default props => split(props.filters).map(row => (
   <div>
     <p className="title"> {parseTitle(row.key)}</p>
-    { items(row.value) }
+    { items(row.key, row.value, props.search) }
     <style jsx>
       {`
             .title {
@@ -39,7 +40,8 @@ export default props => split(props.filters).map(row => (
             }
             .option {
               font-size: 15px;
-              margin-bottom: 5px;
+              margin-bottom: 5px;  
+              display:block;    
             }
             .option:hover {
               cursor: pointer;
