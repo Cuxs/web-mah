a3d086266854b95edbf5e686189576493a62f94d
Franco Catena <francocatena@gmail.com>
2018-05-31 10:26:35 -0300

    New deploy approach

--- build.sh
+++ build.sh
@@ -3,14 +3,7 @@
 set -eu
 
 npm install
-
-if [ -d build ]; then
-  mv build build_old
-fi
-
 npm run build
 
-if [ -d build_old ]; then
-  rsync -a --ignore-existing build_old/ build/
-  rm -rf build_old
-fi
+mkdir -p prod
+rsync -a build/ prod/
