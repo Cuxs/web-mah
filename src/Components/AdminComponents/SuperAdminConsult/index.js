375d8241e22e52abc443d003b7d0c627cbb68fa0
Rodrigo Valles <rodrigovalles@MacBook-Pro-de-Rodrigo.local>
2018-08-07 12:46:22 -0300

    feat(userConsult and superAdminConsult): add consult prices

--- src/Components/AdminComponents/SuperAdminConsult/index.js
+++ src/Components/AdminComponents/SuperAdminConsult/index.js
@@ -1,95 +1,49 @@
 /* eslint react/jsx-filename-extension: 0 */
 /* eslint react/prop-types: 0 */
 
-import React, { Component } from 'react';
-import { Col, Row, Button, Label, FormGroup, ModalHeader, Modal, ModalBody, ModalFooter } from 'reactstrap';
-import { graphql, compose } from 'react-apollo';
-import { parse } from 'query-string';
+import React from 'react';
+import { Col, Row, FormGroup, Label } from 'reactstrap';
+import { graphql, compose, withApollo } from 'react-apollo';
 import { branch, renderComponent } from 'recompose';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
-import _ from 'lodash';
-import ScrollToTop from 'react-scroll-up';
-import { animateScroll as scroll } from 'react-scroll';
 import ReactGA from 'react-ga';
+
 import AdminBar from '../../../stories/AdminBar';
-
-import SuperAdminSideBar from '../../../stories/SuperAdminSideBar';
-import { uploadAgencyImages } from '../../../Modules/fetches';
-import parseError from '../../../Modules/errorParser';
-import Breadcrumb from '../../../stories/BreadCrum';
-
-import {
-  AllBrandsQuery,
-  GroupsQuery,
-  ModelsQuery,
-  YearsQuery,
-} from '../../../ApolloQueries/TautosQuery';
-
+import SuperAdminSideBar from '../../../stories/UserSideBar';
+import { isAdminLogged } from '../../../Modules/sessionFunctions';
+import { AllBrandsQuery, GroupsQuery, ModelsQuery, YearsQuery } from '../../../ApolloQueries/TautosQuery';
+import { prepareArraySelect, thousands, generateYearPerModel } from '../../../Modules/functions';
 import LoginComponent from '../../../stories/LoginComponent';
-import { isUserLogged } from '../../../Modules/sessionFunctions';
-import {
-  thousands,
-  generateYearPerModel,
-  prepareArraySelect,
-} from '../../../Modules/functions';
+
 
 const renderForUnloggedUser = (component, propName = 'data') =>
-  branch(props => !isUserLogged(), renderComponent(component));
+  branch(
+    props => !isAdminLogged(),
+    renderComponent(component),
+  );
 
-class SuperAdminConsult extends Component {
+class SuperAdminConsult extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       brand: '',
       group: '',
       codia: '',
-      year: '',
-      price: '',
       Groups: [],
       Models: [],
       Prices: [],
-      carError: false,
-      stateError: false,
+
     };
-    ReactGA.pageview('/CREAR-PUBLICACION');
-    this.next = this.next.bind(this);
+    ReactGA.pageview('/USUARIO-CONSULTA');
   }
 
-  componentWillReceiveProps({ agencyData }) {
-    if (!agencyData.loading) {
-      const {
-        agencyAdress,
-        agencyEmail,
-        agencyName,
-        agencyPhone,
-        bannerImage,
-        profileImage,
-      } = agencyData.User;
-      scroll.scrollToTop({ duration: 300 });
-      this.setState({
-        agencyAdress,
-        agencyEmail,
-        agencyName,
-        agencyPhone,
-        previewProfileImage: _.isNull(profileImage) ? 'default.jpg' : profileImage,
-        previewBannerImage: _.isNull(bannerImage) ? 'default.jpg' : bannerImage,
-      });
-    }
-  }
   onChangeBrand(newBrand) {
     this.setState({
       brand: newBrand,
-      brandName:
-        newBrand !== null
-          ? _.find(this.props.ta3AllBrands.AllBrands, ['ta3_nmarc', newBrand])
-            .ta3_marca
-          : '',
       group: '',
       codia: '',
       Models: [],
-      modelName: '',
-      groupName: '',
       Prices: [],
       year: '',
       priceSuggested: '',
@@ -106,13 +60,9 @@ class SuperAdminConsult extends Component {
   onChangeGroup(newGroup) {
     this.setState({
       group: newGroup,
-      groupName:
-        newGroup !== null
-          ? _.find(this.state.Groups, ['gru_cgrup', newGroup]).gru_ngrup
-          : '',
-      modelName: '',
       Prices: [],
       year: '',
+      codia: '',
       priceSuggested: '',
     });
     this.props.client
@@ -128,10 +78,6 @@ class SuperAdminConsult extends Component {
   onChangeModel(newModel) {
     this.setState({
       codia: newModel,
-      modelName:
-        newModel !== null
-          ? _.find(this.state.Models, ['ta3_codia', newModel]).ta3_model
-          : '',
     });
     this.props.client
       .query({
@@ -160,141 +106,100 @@ class SuperAdminConsult extends Component {
   }
 
   render() {
-    const { history, location } = this.props;
+    const { history, location, ta3AllBrands: { AllBrands } } = this.props;
     return (
       <div>
-        <AdminBar history={this.props.history} />
+        <AdminBar history={history} />
         <div className="container">
           <Row>
             <Col lg="3" md="12" sm="12" xs="12">
-              <SuperAdminSideBar history={this.props.history} location={this.props.location} />
+              <SuperAdminSideBar history={history} location={location} />
             </Col>
-            <Col lg="9" md="12" sm="12" xs="12" className="mt-4">
-              <Breadcrumb history={this.props.history} />
+            <Col lg="9" md="12" sm="12" className="mt-4">
               <Row>
-                <Col lg="6" md="12" sm="12" xs="12" className="container-data-input-group">
-                  <FormGroup>
-                    <Label for="exampleSelect">¿Cuál es la marca?</Label>
-                    <Select
-                      id="brand-select"
-                      ref={(ref) => {
-                        this.select = ref;
-                      }}
-                      onBlurResetsInput={false}
-                      onSelectResetsInput={false}
-                      options={prepareArraySelect(
-                       // AllBrands,
-                        'ta3_nmarc',
-                        'ta3_marca',
-                      )}
-                      simpleValue
-                      clearable
-                      name="selected-state"
-                      value={this.state.brand}
-                      placeholder="Selecciona una marca"
-                      onChange={newValue => this.onChangeBrand(newValue)}
-                      searchable
-                      noResultsText="No se encontraron resultados"
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="exampleSelect">¿Cuál es el modelo?</Label>
-                    <Select
-                      id="groups-select"
-                      ref={(ref) => {
-                        this.select = ref;
-                      }}
-                      onBlurResetsInput={false}
-                      onSelectResetsInput={false}
-                      options={prepareArraySelect(
-                        this.state.Groups,
-                        'gru_cgrup',
-                        'gru_ngrup',
-                      )}
-                      simpleValue
-                      clearable
-                      name="selected-state"
-                      value={this.state.group}
-                      placeholder="Selecciona un modelo"
-                      onChange={newValue => this.onChangeGroup(newValue)}
-                      searchable
-                      noResultsText="No se encontraron resultados"
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="exampleSelect">¿Cuál es la versión?</Label>
-                    <Select
-                      id="models-select"
-                      ref={(ref) => {
-                        this.select = ref;
-                      }}
-                      onBlurResetsInput={false}
-                      onSelectResetsInput={false}
-                      options={prepareArraySelect(
-                        this.state.Models,
-                        'ta3_codia',
-                        'ta3_model',
-                      )}
-                      simpleValue
-                      clearable
-                      name="selected-state"
-                      value={this.state.codia}
-                      placeholder="Selecciona un tipo"
-                      onChange={newValue => this.onChangeModel(newValue)}
-                      searchable
-                      noResultsText="No se encontraron resultados"
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Label for="exampleSelect">¿Cuál es el año?</Label>
-                    <Select
-                      disabled={this.state.codia === ''}
-                      id="year-select"
-                      ref={(ref) => {
-                        this.select = ref;
-                      }}
-                      onBlurResetsInput={false}
-                      onSelectResetsInput={false}
-                      options={generateYearPerModel(this.state.Prices)}
-                      simpleValue
-                      clearable
-                      required
-                      name="selected-state"
-                      value={this.state.year}
-                      placeholder="Selecciona un año"
-                      onChange={newValue => this.onChangeYear(newValue)}
-                      searchable
-                      noResultsText="No se encontraron resultados"
-                    />
-                  </FormGroup>
-                  {this.state.priceSuggested && (
-                    <p>
-                      Precio Sugerido: <b>{this.state.priceSuggested}</b>
-                    </p>
-                  )}
+                <Col lg="6" md="8" sm="12" className="container-data-input-group mv-15">
+                  <div className="card p-4" style={{ height: '100%' }}>
+                    <FormGroup>
+                      <Label for="exampleSelect">¿Cuál es la marca?</Label>
+                      <Select
+                        id="brand-select"
+                        onBlurResetsInput={false}
+                        onSelectResetsInput={false}
+                        options={prepareArraySelect(AllBrands, 'ta3_nmarc', 'ta3_marca')}
+                        simpleValue
+                        clearable
+                        name="selected-state"
+                        value={this.state.brand}
+                        placeholder="Selecciona una marca"
+                        onChange={newValue => this.onChangeBrand(newValue)}
+                        searchable
+                        noResultsText="No se encontraron resultados"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="exampleSelect">¿Cuál es el modelo?</Label>
+                      <Select
+                        id="groups-select"
+                        onBlurResetsInput={false}
+                        onSelectResetsInput={false}
+                        options={prepareArraySelect(this.state.Groups, 'gru_cgrup', 'gru_ngrup')}
+                        simpleValue
+                        clearable
+                        name="selected-state"
+                        value={this.state.group}
+                        placeholder="Selecciona un modelo"
+                        onChange={newValue => this.onChangeGroup(newValue)}
+                        searchable
+                        noResultsText="No se encontraron resultados"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="exampleSelect">¿Cuál es la versión?</Label>
+                      <Select
+                        id="models-select"
+                        onBlurResetsInput={false}
+                        onSelectResetsInput={false}
+                        options={prepareArraySelect(this.state.Models, 'ta3_codia', 'ta3_model')}
+                        simpleValue
+                        clearable
+                        name="selected-state"
+                        value={this.state.codia}
+                        placeholder="Selecciona un tipo"
+                        onChange={newValue => this.onChangeModel(newValue)}
+                        searchable
+                        noResultsText="No se encontraron resultados"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="exampleSelect">¿Cuál es el año?</Label>
+                      <Select
+                        id="year-select"
+                        onBlurResetsInput={false}
+                        onSelectResetsInput={false}
+                        options={generateYearPerModel(this.state.Prices)}
+                        simpleValue
+                        clearable
+                        required
+                        name="selected-state"
+                        value={this.state.year}
+                        placeholder="Selecciona un año"
+                        onChange={newValue => this.onChangeYear(newValue)}
+                        searchable
+                        noResultsText="No se encontraron resultados"
+                      />
+                    </FormGroup>
+                    <p>Precio Revista: <b>{this.state.priceSuggested}</b></p>
+                  </div>
                 </Col>
               </Row>
             </Col>
           </Row>
-          <ScrollToTop showUnder={320} >
-            <img style={{ width: '30px' }} src="/assets/images/icon-arrow-top.svg" alt="Inicio" />
-          </ScrollToTop>
         </div>
-        <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
-          <ModalHeader toggle={this.toggleModal}>{this.state.responseTitle}</ModalHeader>
-          <ModalBody>
-            <div className="col-md-6 offset-md-3">
-              <h5>{this.state.responseMsg}</h5>
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={() => this.toggleModal()}>OK</Button>
-          </ModalFooter>
-        </Modal>
       </div>
     );
   }
 }
+
 
 const WithAllBrands = graphql(AllBrandsQuery, {
   name: 'ta3AllBrands',
@@ -302,7 +207,8 @@ const WithAllBrands = graphql(AllBrandsQuery, {
 
 const withData = compose(
   WithAllBrands,
-  renderForUnloggedUser(LoginComponent, 'userProfile'),
+  renderForUnloggedUser(LoginComponent, 'userConsult'),
 );
 
-export default withData(SuperAdminConsult);
+
+export default withApollo(withData(SuperAdminConsult));
