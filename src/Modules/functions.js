6c2845c95dc885092306400b16a2657d6a96bdb3
Mariano Fliri <mfliri@as-one.com.ar>
2018-11-07 14:25:13 -0300

    Fix thousands function

--- src/Modules/functions.js
+++ src/Modules/functions.js
@@ -1,31 +1,10 @@
 import format from 'format-number';
 import _ from 'lodash';
 import moment from 'moment';
+import format from 'format-number';
 
-export const thousands = (number, decimals, decPoint = ',', thousandsSep = '.') =>
-  format({ round: decimals, integerSeparator: thousandsSep, decimal: ',' })(number);
-  // if (!number) { return false; }
-  // decimals = decimals || 0;
-  // number = parseFloat(number);
 
-  // if (!decPoint || !thousandsSep) {
-  //   decPoint = '.';
-  //   thousandsSep = ',';
-  // }
-
-  // const roundedNumber = `${Math.round(Math.abs(number) * (`1e${decimals}`))}`;
-  // let numbersString = decimals ? roundedNumber.slice(0, decimals * -1) : roundedNumber;
-  // const decimalsString = decimals ? roundedNumber.slice(decimals * -1) : '';
-  // let formattedNumber = '';
-
-  // while (numbersString.length > 3) {
-  //   formattedNumber += thousandsSep + numbersString.slice(-3);
-  //   numbersString = numbersString.slice(0, -3);
-  // }
-
-  // return (number < 0 ? '-' : '') + numbersString + formattedNumber;
-  // return (number < 0 ? '-' : '') + numbersString + formattedNumber + (decimalsString ? (decPoint + decimalsString) : '');
-
+export const thousands = (number, decimals, decPoint = ',', thousandsSep = '.') => format({ round: decimals, integerSeparator: thousandsSep, decimal: ',' })(number);
 export const prepareArraySelect = (array, rowValue, rowLabel) => {
   const newArray = [];
 
